844c8d5b2bb56fa38b82df142e70b994b887a8a3
Yellowbebo <bbb4bebo@gmail.com>
2025-03-27 12:18:23 +0300

    Part 1.

--- src/main/java/com/example/CustomItems.java
+++ src/main/java/com/example/CustomItems.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item.Settings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registry;
 
 public class CustomItems {
-    public static final Item MY_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item MY_ITEM = new Item(new Item.Settings());
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("com.example", "my_item"), MY_ITEM);
